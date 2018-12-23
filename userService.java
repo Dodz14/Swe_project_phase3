571c0ed60adabea75f2d479064f65858802a9054
Sherif-Gameel <45367916+Sherif-Gameel@users.noreply.github.com>
2018-12-23 16:10:40 +0200

    login function and first step of post and search

--- userService.java
+++ userService.java
@@ -36,7 +36,65 @@ public class userService {
 
 	}
 
-	
+	public void Login() 
+	  {
+		  String phone_no = new String();
+		  String password = new String();
+		  userController Again = new userController();
+		  
+		  Scanner in_info = new Scanner(System.in);
+		  System.out.println("Please enter your password :\n");
+		  password = in_info.nextLine();
+		  Scanner in_info1 = new Scanner(System.in);
+		  System.out.println("Please enter your phone number :\n");
+		  phone_no = in_info1.nextLine();
+		  
+		  for (int i=0 ; i<users.size() ; i++) 
+		  {
+			  String x = new String();
+			  x = users.get(i).password;
+			  String y = new String();
+			  y = users.get(i).phone_num;
+			  if (x.equals(password)  &&  y.equals(phone_no)) 
+			  {
+				  System.out.println("Login successfully");
+				  return;
+			  }
+		  }
+			  
+			  
+				  System.out.println("1- login again \n 2- register");
+				  int choice0;
+				  choice0 = in_info.nextInt();
+				  
+				  if (choice0==1) 
+				  {
+					  Login();
+				  }
+				  else if (choice0==2)
+				  {
+					  registerUser();
+				  }
+				  
+				  else 
+					  Again.receive_request();	  
+	}
+
+	  
+
+	  public void uploadMissItem() 
+	  {
+		  itemController obj = new itemController();
+		  obj.receive_request();
+		  
+
+	  }
+
+	  public void Check_item() 
+	  {
+    	 itemController ob = new itemController();
+    	 ob.receive_request();
+	  }
 
 
 
